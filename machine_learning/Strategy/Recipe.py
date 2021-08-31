eb830f6cc230b7c14947d2751fdf391eb396d64e
yoyo-mical <59496611+yoyo-mical@users.noreply.github.com>
2021-08-31 22:18:20 +0800

    stategy_Fallingprobe update完成

--- machine_learning/Strategy/Recipe.py
+++ machine_learning/Strategy/Recipe.py
@@ -1,4 +1,4 @@
-import pymysql.cursors
+import pymysql
 
 
 def recipe_buy(datelist, codelist, moneylist, buymark):
@@ -15,19 +15,19 @@ def recipe_buy(datelist, codelist, moneylist, buymark):
     cursor = db.cursor()
 
     if buymark == 'equmoney_bycode':
-        for i in len(datelist):
-            for j in len(codelist[i]):
+        for i in range(len(datelist)):
+            for j in range(len(codelist[i])):
                 sql_recipe = "insert ignore into deal_recipe (action,state_dt,stock_code,money)" \
-                             "value ({},{},{},{})" \
+                             "value ('{}','{}','{}',{})" \
                     .format('buy', datelist[i], codelist[i][j], moneylist[i][0])
                 cursor.execute(sql_recipe)
                 tup_buy = ('buy', datelist[i], codelist[i][j], moneylist[i][0])
                 buyactlist.append(tup_buy)
     elif buymark == 'defmoney_bycode':
-        for i in len(datelist):
-            for j in len(codelist[i]):
+        for i in range(len(datelist)):
+            for j in range(len(codelist[i])):
                 sql_recipe = "insert ignore into deal_recipe (action,state_dt,stock_code,money)" \
-                             "value ({},{},{},{})" \
+                             "value ('{}','{}','{}',{})" \
                     .format('buy', datelist[i], codelist[i][j], moneylist[i][j])
                 cursor.execute(sql_recipe)
                 tup_buy = ('buy', datelist[i], codelist[i][j], moneylist[i][j])
@@ -45,10 +45,10 @@ def recipe_sell(datelist, codelist):
     db = pymysql.connect(host='127.0.0.1', user='root', passwd='123456', db='test', charset='utf8')
     cursor = db.cursor()
 
-    for i in len(datelist):
-        for j in len(codelist[i]):
+    for i in range(len(datelist)):
+        for j in range(len(codelist[i])):
             sql_recipe = "insert ignore into deal_recipe (action,state_dt,stock_code)" \
-                         "value ({},{},{})" \
+                         "value ('{}','{}','{}')" \
                 .format('sell', datelist[i], codelist[i][j])
             cursor.execute(sql_recipe)
             tup_sell = ('sell', datelist[i], codelist[i][j])
--- machine_learning/Strategy/stategy_Fallingprobe.py
+++ machine_learning/Strategy/stategy_Fallingprobe.py
@@ -1,52 +1,102 @@
 import pymysql.cursors
 import pandas as pd
 import datetime
+from machine_learning.Strategy import Recipe
 
 # 寻找连续下跌stock,进行买卖回测；
 
-falllist = []
-db_name = '主板'
-db = pymysql.connect(host='127.0.0.1', user='root', passwd='123456', db=db_name, charset='utf8')
+# 生成时间序列：
+
+
+state_dt = '2021-07-03'
+para_window = 35
+step = 7
+date_seq ={}
+
+date_start = (datetime.datetime.strptime(state_dt, '%Y-%m-%d')
+              - datetime.timedelta(days=para_window)).strftime('%Y-%m-%d')
+date_end = state_dt
+
+db = pymysql.connect(host='127.0.0.1', user='root', passwd='123456', db='test', charset='utf8')
 cursor = db.cursor()
 
+sql_clear = "truncate TABLE deal_recipe"
+cursor.execute(sql_clear)
 
-sql1 = "select table_name from information_schema.tables where table_schema='{}'".format(db_name)
-cursor.execute(sql1)
-result1 = cursor.fetchall()
-df1 = pd.DataFrame(result1)
-tabel_list = df1[0].to_list()
+sql_cal = "select state_dt from trade_cal where state_dt >= '{date_start}' and state_dt <= '{date_end}' " \
+          "and is_open = 1 order by state_dt asc".format(date_start = date_start,date_end = date_end )
 
+cursor.execute(sql_cal)
+result_cal = cursor.fetchall()
+df_cal = pd.DataFrame(result_cal)
+date_temp = df_cal[0].to_list()
 
-date_ = '2021-08-28'
-for tb in tabel_list:
-    sql2 = "select stock_code from {} group by stock_code".format(tb)
-    cursor.execute(sql2)
-    result2 = cursor.fetchall()
-    df2 = pd.DataFrame(result2)
-    codelist = df2[0].to_list()
-    for code in codelist:
-        n = 0
-        try:
-            sql3 = "select pct_change from {tb} where stock_code = '{code}' " \
-                   "and state_dt< '{date_}'order by state_dt desc".format(tb=tb, code=code, date_=date_)
-            cursor.execute(sql3)
-            result3 = cursor.fetchall()
-            df3 = pd.DataFrame(result3)
-            pctlist = df3[0].to_list()
-            for pct in pctlist:
-                if pct <= 0:
-                    n = n + 1
-                else:
-                    break
-            tup = (code, n)
-            falllist.append(tup)
-        except:
-            print(tb, code)
-
-print(falllist)
-df4 = pd.DataFrame(falllist)
-print(df4)
-print(df4.sort_values(by=[1],ascending=False))
+for x in range(0,len(date_temp),step+1):
+    if x + step < len(date_temp):
+        date_seq.update({date_temp[x].strftime('%Y-%m-%d'):date_temp[x+step].strftime('%Y-%m-%d')})
+    else:
+        break
 
 db.commit()
 db.close()
+print(date_seq)
+
+
+falllist = []
+
+db_name = '科创板'
+
+for date_buy,date_sell in date_seq.items():
+    db = pymysql.connect(host='127.0.0.1', user='root', passwd='123456', db=db_name, charset='utf8')
+    cursor = db.cursor()
+
+    sql1 = "select table_name from information_schema.tables where table_schema='{}'".format(db_name)
+    cursor.execute(sql1)
+    result1 = cursor.fetchall()
+    df1 = pd.DataFrame(result1)
+    tabel_list = df1[0].to_list()
+
+    toplist = []
+
+    for tb in tabel_list:
+        sql2 = "select stock_code from {} group by stock_code".format(tb)
+        cursor.execute(sql2)
+        result2 = cursor.fetchall()
+        df2 = pd.DataFrame(result2)
+        codelist = df2[0].to_list()
+        for code in codelist:
+            n = 0
+            try:
+                sql3 = "select pct_change from {tb} where stock_code = '{code}' " \
+                       "and state_dt< '{date_}'order by state_dt desc".format(tb=tb, code=code, date_=date_buy)
+                cursor.execute(sql3)
+                result3 = cursor.fetchall()
+                df3 = pd.DataFrame(result3)
+                pctlist = df3[0].to_list()
+                for pct in pctlist:
+                    if pct <= 0:
+                        n = n + 1
+                    else:
+                        break
+                tup = (tb, code, n)
+                falllist.append(tup)
+            except:
+                print(tb, code)
+
+    # print(falllist)
+    df4 = pd.DataFrame(falllist)
+    df4 = df4.sort_values(by=[2], ascending=False)
+    print(df4)
+    toplist = df4.iloc[0:5,1].to_list()
+    datelist_buy = [date_buy]
+    datelist_sell = [date_sell]
+    stocklist = [toplist]
+    moneylist = [[10000]]
+    db.commit()
+    db.close()
+    Recipe.recipe_buy(datelist_buy,stocklist,moneylist,'equmoney_bycode')
+    Recipe.recipe_sell(datelist_sell,stocklist)
+
+
+
+
